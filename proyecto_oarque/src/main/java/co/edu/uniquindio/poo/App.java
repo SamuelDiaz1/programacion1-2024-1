d57b3dcbd313e2c5d6d15c5cdd06be0e3540be94
SamuelDiaz1 <161249988+SamuelDiaz1@users.noreply.github.com>
2024-04-05 17:27:03 -0500

    updated

--- proyecto_oarque/src/main/java/co/edu/uniquindio/poo/App.java
+++ proyecto_oarque/src/main/java/co/edu/uniquindio/poo/App.java
@@ -6,7 +6,7 @@ package co.edu.uniquindio.poo;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Worlds!");
+        System.out.println("Hello World!");
     }
 
 }
